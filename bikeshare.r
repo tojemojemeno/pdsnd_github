5dcf65e02306d95bdd7ca9d66cba5e08a0b1f677
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-03-25 13:28:36 +0100

    refactor: Start comments with capital letters

--- bikeshare.r
+++ bikeshare.r
@@ -7,21 +7,21 @@ head(ny)
 head(wash)
 head(chi)
 
-# question 1 --------------------------------------------------------------
+# Question 1 --------------------------------------------------------------
 #How many times were the most popular start stations used?
 
 
-#dataframes for start stations and their counts in each city in decreasing order
+#Dataframes for start stations and their counts in each city in decreasing order
 t_wash<-data.frame(count=sort(table(wash$Start.Station), decreasing =TRUE))
 t_chi<-data.frame(count=sort(table(chi$Start.Station), decreasing =TRUE))
 t_ny<-data.frame(count=sort(table(ny$Start.Station), decreasing =TRUE))
 
-#top 10 start stations in each city
+#Top 10 start stations in each city
 t1_wash<-head(t_wash,10)
 t1_chi<-head(t_chi,10)
 t1_ny<-head(t_ny,10)
 
-#visualization
+#Visualization
 ggplot(aes(x=count.Var1, y=count.Freq), data=t1_wash)+
   geom_bar(stat="identity")+
   theme_minimal()+
@@ -44,24 +44,24 @@ ggplot(aes(x=count.Var1, y=count.Freq), data=t1_ny)+
        y="Count",
        title="Top 10 start stations in NYC")
 
-#summary
+#Summary
 summary(t1_wash)
 summary(t1_chi)
 summary(t1_ny)
 
 #The top 10 start stations for Washington lie within the range of 823(Eastern Market Metro/Pennsylvania Ave & 7th St SE) to 1700(Columbus Circle/Union Station). For Chicago the range lies within 81(Franklin St & Monroe St)and 210(Streeter Dr & Grand Ave). For NYC it is 288(Greenwich Ave & 8 Ave) and 592(Pershing Square North).
 
-# question 2 --------------------------------------------------------------
+# Question 2 --------------------------------------------------------------
 #What is the average year of birth for each gender?
 
-# how many people of each gender are there?
+# How many people of each gender are there?
 table(chi$Gender)
 table(ny$Gender)
-#summary by gender
+#Summary by gender
 by(chi$Birth.Year, chi$Gender, summary)
 by(ny$Birth.Year, ny$Gender, summary)
 
-#visualization
+#Visualization
 ggplot(aes(y=Birth.Year), data= ny)+
   geom_boxplot()+
   facet_wrap(~Gender)+
@@ -75,20 +75,20 @@ ggplot(aes(y=Birth.Year), data= chi)+
 
 #The data is not symmetric, so median is the better way to count the average. In Chicago the median year of birth is 1986 for women and 1983 for men. In NYC 1982 is for women and 1980 for men. In Chicago, people who didn't choose their gender also didn't mention their year of birth (except for one observation). In NYC, however, there is a whole group of people with its own average of 1979, who didn't select the binary gender options, but mentioned their year of birth.
 
-# question 3 --------------------------------------------------------------
+# Question 3 --------------------------------------------------------------
 #What is the most common day of week?
 
-# converting date to weekday
+# Converting date to weekday
 ny$day <-weekdays(as.Date(ny$Start.Time))
 wash$day<-weekdays(as.Date(wash$Start.Time))
 chi$day<-weekdays(as.Date(chi$Start.Time))
 
-#creating dataframe with weekdays and frequency
+# Creating dataframe with weekdays and frequency
 ny_day<-data.frame(table(ny$day))
 wash_day<-data.frame(table(wash$day))
 chi_day<-data.frame(table(chi$day))
 
-#visualization
+# Visualization
 ny_day %>% 
   mutate(Var1=fct_relevel(Var1,"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
                           "Friday", "Saturday")) %>% 
